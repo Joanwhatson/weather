cd674ca4bace18964b3721182a15337d9230f273
Joanwhatson <76024688+Joanwhatson@users.noreply.github.com>
2022-04-12 14:10:17 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect} from "react"
+import axios from 'axios'
+function App() {
+  const [data, setData] = useState([])
+  const[city, setCity] = useState('')
+  let call = () =>{
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7b825cfac5aaa852f35915bfe0752ab6`)
+    .then(res=>{setData(res.data)}) 
+    console.log(data)
+  }
+  let submit = (e) =>{
+    e.preventDefault()
+    call()
+  }
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+      <input placeholder="Search.." onChange={ e => {setCity(e.target.value)}}/>
+      </form>
+            <h1>{data.sys?.country} {data.name}</h1>
+            <h2>{data.main?.temp}</h2>
+            <h3>{data.weather?.length && data.weather[0]?.main}</h3>
+            <p>{data.main?.temp_min}  {data.main?.temp_max}</p>
+      
+    </div>
+  );
+}
+
+export default App;
